46ce3d1f57421757ceb562b00249d3c10ca02a62
unknown <arturo.mayorga@protonmail.com>
2024-07-20 23:10:16 -0400

    setting up exports skeleton

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { userConfigHandler } from './usrcfg';
+export { userDataHandler } from './usrdata';
+export { getDocument } from './ftchdata';
+export { getDocument as getDataLakeDocument } from './dtlkdata';
